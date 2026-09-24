a456c6e441f4c152cc80556d4208e2f56db1ddee
Viktor Zahorodnii <post.vittorius@gmail.com>
2026-09-24 17:24:25 +0300

    WIP

--- src/environment.rs
+++ src/environment.rs
@@ -16,37 +16,89 @@ pub type EnvShared = Rc<RefCell<Env>>;
 #[derive(Debug)]
 // TODO: rename into Env
 pub struct Env {
+    // values: HashMap<String, Value>,
+    // enclosing: Option<EnvShared>,
+    // return_value: Option<Value>,
+    data: Rc<RefCell<EnvData>>,
+}
+
+#[derive(Debug, Default)]
+struct EnvData {
     values: HashMap<String, Value>,
-    enclosing: Option<EnvShared>,
+    enclosing: Option<Rc<RefCell<EnvData>>>,
     return_value: Option<Value>,
 }
 
-// TODO: rename into Env
+impl EnvData {
+    fn new_enclosed_with(env: Rc<RefCell<EnvData>>) -> Self {
+        Self {
+            enclosing: Some(env),
+            ..EnvData::default()
+        }
+    }
+
+    fn get(&self, name: &str) -> Option<Value> {
+        if let Some(value) = self.values.get(name) {
+            Some(value.clone())
+        } else if let Some(enclosing) = &self.enclosing {
+            enclosing.borrow().get(name)
+        } else {
+            None
+        }
+    }
+
+    fn get_at(&self, distance: usize, name: &str) -> Option<Value> {
+        if distance == 0 {
+            self.get(name)
+        } else if let Some(enclosing) = &self.enclosing {
+            enclosing.borrow().get_at(distance - 1, name)
+        } else {
+            unreachable!(
+                "Wrong var resolution distance {} when enclosing env is missing",
+                distance
+            )
+        }
+    }
+
+    fn assign(&mut self, name: &Token, value: Value) -> Result<Value, RuntimeError> {
+        if self.values.contains_key(&name.lexeme) {
+            self.values.insert(name.lexeme.clone(), value.clone());
+            Ok(value)
+        } else if let Some(enclosing) = &self.enclosing {
+            enclosing.borrow_mut().assign(name, value)
+        } else {
+            Err(RuntimeError::new(
+                name,
+                &format!("Undefined variable \"{}\"", name.lexeme),
+            ))
+        }
+    }
+}
+
 impl Env {
     // TODO: delete this method and move its logic to `wrapped`
     pub fn new() -> Self {
         Self {
-            values: HashMap::new(),
-            enclosing: None,
-            return_value: None,
+            data: Rc::new(RefCell::new(EnvData::default())),
         }
     }
 
-    pub fn with_enclosing(enclosing: EnvShared) -> Self {
+    // TODO: rename to enclosed_with
+    pub fn with_enclosing(enclosing: &Env) -> Self {
         Self {
-            enclosing: Some(enclosing),
-            values: HashMap::new(),
-            return_value: None,
+            data: Rc::new(RefCell::new(EnvData::new_enclosed_with(Rc::clone(
+                &enclosing.data,
+            )))),
         }
     }
 
     // TODO: rename to `new_shared`
-    pub fn wrapped(self) -> EnvShared {
-        Rc::new(RefCell::new(self))
-    }
+    // pub fn wrapped(self) -> EnvShared {
+    //     Rc::new(RefCell::new(self))
+    // }
 
     pub fn define(&mut self, name: String, value: Value) {
-        self.values.insert(name, value);
+        self.data.borrow_mut().values.insert(name, value);
     }
 
     // TODO: use when BareEnv is renamed to Env
@@ -61,51 +113,60 @@ impl Env {
     // The environment could be HashMap<String, Rc<RefCell<Value>>> but it seems more natural to move the
     // value/reference duality to the Value itself (see Value definition.)
     pub fn get(&self, name: &str) -> Option<Value> {
-        self.values.get(name).cloned().or_else(|| {
-            if let Some(enclosing) = &self.enclosing {
-                enclosing.borrow().get(name)
-            } else {
-                None
-            }
-        })
+        // self.values.get(name).cloned().or_else(|| {
+        //     if let Some(enclosing) = &self.enclosing {
+        //         enclosing.borrow().get(name)
+        //     } else {
+        //         None
+        //     }
+        // })
+        // self.data.borrow().values.get(name).or_else(|| {
+        //     if let Some(enclosing) = s
+        // })
+        self.data.borrow().get(name)
     }
 
     // See the `get` method note about not returning Option<&Value> here.
     pub fn get_at(&self, distance: usize, name: &str) -> Option<Value> {
-        if distance == 0 {
-            self.get(name)
-        } else {
-            self.ancestor(NonZeroUsize::new(distance).expect("The distance must be non-zero"))
-                .borrow()
-                .values
-                .get(name)
-                .cloned()
-        }
+        // if distance == 0 {
+        //     self.get(name)
+        // } else {
+        //     self.ancestor(NonZeroUsize::new(distance).expect("The distance must be non-zero"))
+        //         .borrow()
+        //         .values
+        //         .get(name)
+        //         .cloned()
+        // }
+        self.data.borrow().get_at(distance, name)
     }
 
     // TODO: return Result<(), RuntimeError> here and make the caller mess with Value cloning
     // Using &Token here is correct because this method semantics is similar to .get() or .contains_key():
     // you cannot assign what hasn't been defined, so .assign() must not consume the key but rather borrow it.
-    pub fn assign(&mut self, name: &Token, value: Value) -> Result<Value, RuntimeError> {
-        use std::collections::hash_map::Entry;
+    //     pub fn assign(&mut self, name: &Token, value: Value) -> Result<Value, RuntimeError> {
+    //         use std::collections::hash_map::Entry;
+    //
+    //         // TODO: try using .contains_key() to avoid premature cloning of `name.lexeme`
+    //         match self.values.entry(name.lexeme.clone()) {
+    //             Entry::Occupied(mut occupied_entry) => {
+    //                 occupied_entry.insert(value.clone()); // TODO: avoid this clone making the caller do this
+    //                 Ok(value)
+    //             }
+    //             Entry::Vacant(_) => {
+    //                 if let Some(enclosing) = &mut self.enclosing {
+    //                     enclosing.borrow_mut().assign(name, value)
+    //                 } else {
+    //                     Err(RuntimeError::new(
+    //                         name,
+    //                         &format!("Undefined variable \"{}\"", name.lexeme),
+    //                     ))
+    //                 }
+    //             }
+    //         }
+    //     }
 
-        // TODO: try using .contains_key() to avoid premature cloning of `name.lexeme`
-        match self.values.entry(name.lexeme.clone()) {
-            Entry::Occupied(mut occupied_entry) => {
-                occupied_entry.insert(value.clone()); // TODO: avoid this clone making the caller do this
-                Ok(value)
-            }
-            Entry::Vacant(_) => {
-                if let Some(enclosing) = &mut self.enclosing {
-                    enclosing.borrow_mut().assign(name, value)
-                } else {
-                    Err(RuntimeError::new(
-                        name,
-                        &format!("Undefined variable \"{}\"", name.lexeme),
-                    ))
-                }
-            }
-        }
+    pub fn assign(&self, name: &Token, value: Value) -> Result<Value, RuntimeError> {
+        self.data.borrow_mut().assign(name, value)
     }
 
     // TODO: consume `name` and make the caller .clone()
